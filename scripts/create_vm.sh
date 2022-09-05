49d3ffb86b79bc34350328383f938cce856e9738
Franco Piccolo <francopiccolo@gmail.com>
2022-09-05 12:31:50 +0300

    migrated push to artifact registry

--- scripts/create_vm.sh
+++ scripts/create_vm.sh
@@ -1,6 +1,5 @@
 gcloud compute instances create-with-container twitter-bot \
-    --container-image=us-east1-docker.pkg.dev/manu-twitter-bot/twitter-bot/image \
-    --container-mount-host-path mount-path=/bot/config,host-path=/tmp/bot/config,mode=ro \
+    --container-image=gcr.io/manu-twitter-bot/twitter-bot/image \
     --container-restart-policy=always \
     --zone=us-east1-b \
     --machine-type=e2-small
