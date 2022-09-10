5ee760efcf7a61cde3bac1d413fa00891ca4af66
Franco Piccolo <francopiccolo@gmail.com>
2022-09-10 22:14:16 +0300

    changed architecture to cloud run

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
 import logging
+
+from flask import Flask
 
 from src.bot import process_accounts
 from src.utils.slack import send_text_message
@@ -8,11 +11,19 @@ logging.basicConfig(
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S')
 
+app = Flask(__name__)
 
-if __name__ == '__main__':
+@app.route("/")
+def run():
     try:
         process_accounts()
-        send_text_message('Bot succeeded')
+        message = 'Bot succeeded'
+        send_text_message(message)
+        return message
     except Exception as e:
         logging.exception('Exception')
         send_text_message('<!channel> Bot failed ' + str(e))
+        return 'Bot failed'
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
